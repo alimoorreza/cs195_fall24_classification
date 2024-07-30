2ecae262c78dca1acbf3759000818f55490ecc9f
Alimoor Reza <142291943+alimoorreza@users.noreply.github.com>
2024-07-30 18:03:56 -0500

    Update alexnet.py

--- networks/alexnet.py
+++ networks/alexnet.py
@@ -1,1 +1,46 @@
+import torch
+import torch.nn as nn
+from torchvision import models
+import pdb
+
+# You can give any name to your new network, e.g., AlexNet.
+# You should load the pretrained AlexNet model from torchvision.models.
+# This model was trained on over a million real-world images from ImageNet.
+# The idea is to bootstrap our CNN network weights with pretrained weights.
+# Our model will converge to a solution faster.
+# This training process is called 'fine-tuning.'
+
+
+class AlexNet(nn.Module):
+    def __init__(self, num_classes, pretrained=True):
+        super(AlexNet, self).__init__()
+        net = models.alexnet(pretrained=True)
+
+        # retained earlier convolutional and pooling layers from AlexNet
+        self.features   = net.features
+        self.avgpool    = net.avgpool
+
+        # added new fully connected layers
+        self.classifier = nn.Sequential(
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 512),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(512, num_classes)
+        )
+
+
+    def forward(self, x):
+        #print("shape of input: ", x.shape)
+        x = self.features(x)
+        #print("output shape (self.features): ", x.shape)
+        x = self.avgpool(x)
+        #print("output shape (self.avgpool): ", x.shape)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        #print("output shape (self.classifier): ", x.shape)
+        return x
+
 
