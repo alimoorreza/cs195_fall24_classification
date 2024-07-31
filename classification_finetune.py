ab39e032b16fac793f8241007c33c43cd01c1dd5
Alimoor Reza <142291943+alimoorreza@users.noreply.github.com>
2024-07-31 03:16:05 -0500

    Add files via upload

--- classification_finetune.py
+++ classification_finetune.py
@@ -1,10 +1,11 @@
-# Step 1: load the Torch library and other utilities
+# load the Torch library and other utilities
 #----------------------------------------------------
 
 import argparse
 import random
 import torch
 import torch.nn as nn
+import torch.backends.cudnn as cudnn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from torch.autograd import Variable
@@ -13,14 +14,22 @@ from torchvision import models
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
-import pandas
+#import pandas
 import time
-import numpy as np
 import os
 import pdb
+import sys
+from config import get_cfg_defaults
+from utils.normalization_utils import get_imagenet_mean_std_normalized
+from networks import alexnet
 
-from config import config_hrnet_v2 as config
-from config import update_config_hrnet_v2 as update_config
+
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "cpu"
+)
+print(f"Using {device} device")
 
 def parse_args():
   
@@ -35,63 +44,54 @@ def parse_args():
                         default=None,
                         nargs=argparse.REMAINDER)
     args = parser.parse_args()
-    update_config(config, args)
-
-    return args
-
-def main():
-
-  # get the arguments after parsing
-  args = parse_args()
-  if args.seed > 0:
-    print('seeding with ', args.seed)
-    random.seed(args.seed)
-    torch.manual_seed(args.seed)
-
-  # cudnn related setting
-  cudnn.benchmark = args.
-  
-'''
-# check to see if torch.cuda is available, otherwise it will use CPU
-device = (
-    "cuda"
-    if torch.cuda.is_available()
-    else "cpu"
-)
-print(f"Using {device} device")
-
-# os.environ["CUDA_VISIBLE_DEVICES"]="0"
-# Step 2: load the dataset
-#--------------------------------------------------------------------------------------------------
-# For fine-tuning with an AlexNet/VGG/ResNet architecture that has been pre-trained using the ImageNet dataset, you need to normalize
-# each image with the given mean and standard deviation.
-transform = transforms.Compose([
-    transforms.Resize((227, 227)),
-    transforms.ToTensor(),
-    transforms.Normalize((.229, .224, .225), (.485, .456, .406)) # ImageNet: mean (R, G, B) and standard deviation (R, G, B)
-])
+    cfg  = get_cfg_defaults()
+    cfg.merge_from_file(args.cfg)
+    cfg.freeze()
+    # print(cfg)
+    
+    return args, cfg
 
 
-train_dir       = '/content/drive/MyDrive/cs195_fall24/datasets/bcdp_v1/train'
-test_dir        = '/content/drive/MyDrive/cs195_fall24/datasets/bcdp_v1/test'
+def prepare_data(config):
+    
+    mean, std = get_imagenet_mean_std_normalized()
+    print(f"mean: {mean}, std: {std}")
+    
+    # For fine-tuning with an AlexNet/VGG/ResNet architecture that has been pre-trained using the ImageNet dataset, 
+    # you need to normalize each image with the given mean and standard deviation.
+    transform = transforms.Compose([
+        transforms.Resize((config.TRAIN.IMAGE_SIZE[0], config.TRAIN.IMAGE_SIZE[1])),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=mean, std=std) # ImageNet: mean (R, G, B) and standard deviation (R, G, B)
+    ])
 
-train_dataset   = datasets.ImageFolder(train_dir, transform=transform) # it loads images from the given directory, subsequently, classes are assigned labels according to the sorted order of the folder names.
-test_dataset    = datasets.ImageFolder(test_dir,  transform=transform) # it loads images from the given directory, subsequently, classes are assigned labels according to the sorted order of the folder names.
+    train_dir       = os.path.join(config.DATASET.ROOT, config.DATASET.TRAIN_SET) #'/content/drive/MyDrive/cs195_fall24/datasets/bcdp_v1/train'
+    test_dir        = os.path.join(config.DATASET.ROOT, config.DATASET.TEST_SET) #'/content/drive/MyDrive/cs195_fall24/datasets/bcdp_v1/test'
 
-N_train         = len(train_dataset)
-N_test          = len(test_dataset)
+    # it loads images from the given directory, subsequently, classes are assigned labels according to the sorted order of the folder names.
+    train_dataset   = datasets.ImageFolder(train_dir, transform=transform) 
+    # it loads images from the given directory, subsequently, classes are assigned labels according to the sorted order of the folder names.
+    test_dataset    = datasets.ImageFolder(test_dir,  transform=transform) 
 
-number_of_classes = 4                    # Headsup! You should change this to the appropriate number when you fine-tune your model on a different dataset.
+    N_train         = len(train_dataset)
+    N_test          = len(test_dataset)
 
-print("Size of train set:", N_train)
-print("Size of test set:",  N_test)
+    print("Size of train set:", N_train)
+    print("Size of test set:",  N_test)
+    
+    # shuffle the images in training set during fine-tuning
+    train_dataloader  = DataLoader(train_dataset, batch_size=config.TRAIN.BATCH_SIZE, shuffle=True) 
+    # you don't need to shuffle test images as they are not used during training
+    test_dataloader   = DataLoader(test_dataset, batch_size=config.TEST.BATCH_SIZE,  shuffle=False) 
+    
 
+    return train_dataloader, test_dataloader
 
-
-# Step 3: Your training and testing functions
-#--------------------------------------------------------------------------------------
-
+    
 def train_loop(dataloader, model, loss_fn, optimizer):
+
+    # for calculating the probability of the network prediction.
+    softmax         = nn.Softmax(dim=1)
 
     size            = len(dataloader.dataset)
     num_batches     = len(dataloader)
@@ -114,7 +114,7 @@ def train_loop(dataloader, model, loss_fn, optimizer):
         pred = model(X)
         loss = loss_fn(pred, y)
 
-        # Backpropagation
+        # backpropagation
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
@@ -134,7 +134,6 @@ def train_loop(dataloader, model, loss_fn, optimizer):
         train_pred_all.append(pred_y) # save predicted output for the current batch
         train_y_all.append(y)         # save ground truth for the current batch
 
-    #pdb.set_trace()
     train_pred_all = torch.cat(train_pred_all) # need to concatenate batch-wise appended items
     train_y_all = torch.cat(train_y_all)
 
@@ -147,6 +146,9 @@ def train_loop(dataloader, model, loss_fn, optimizer):
 
 def test_loop(dataloader, model, loss_fn):
 
+    # for calculating the probability of the network prediction.
+    softmax         = nn.Softmax(dim=1)
+    
     model.eval()                    # set the model to evaluation mode for best practices
 
     size                = len(dataloader.dataset)
@@ -159,24 +161,24 @@ def test_loop(dataloader, model, loss_fn):
     # also serves to reduce unnecessary gradient computations and memory usage for tensors with requires_grad=True
     with torch.no_grad():
 
-      for X, y in dataloader:
+        for X, y in dataloader:
 
-        # ----------- putting data into gpu or sticking to cpu ----------
-        X = X.to(device)     # send data to the GPU device (if available)
-        y = y.to(device)
-        # -----------                                         ----------
+            # ----------- putting data into gpu or sticking to cpu ----------
+            X = X.to(device)     # send data to the GPU device (if available)
+            y = y.to(device)
+            # -----------                                         ----------
 
-        pred = model(X)
-        test_loss += loss_fn(pred, y).item()
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
 
-        # calculate probability and save the outputs for confusion matrix computation
-        pred_prob     = softmax(pred)
-        pred_y        = torch.max(pred_prob, 1)[1]
-        test_correct  = (pred_y == y).sum()
-        correct      += test_correct.data
+            # calculate probability and save the outputs for confusion matrix computation
+            pred_prob     = softmax(pred)
+            pred_y        = torch.max(pred_prob, 1)[1]
+            test_correct  = (pred_y == y).sum()
+            correct      += test_correct.data
 
-        test_pred_all.append(pred_y) # save predicted output for the current batch
-        test_y_all.append(y)         # save ground truth for the current batch
+            test_pred_all.append(pred_y) # save predicted output for the current batch
+            test_y_all.append(y)         # save ground truth for the current batch
 
 
     #pdb.set_trace()
@@ -190,62 +192,99 @@ def test_loop(dataloader, model, loss_fn):
     return test_loss, 100*correct, confusion_matrix(test_y_all.cpu().data, test_pred_all.cpu().data)
 
 
-# Step 4: Use the AlexNet from above
-#--------------------------------------------------------------------------------------------------
+def main():
 
-# You can give any name to your new network, e.g., AlexNet.
-# You should load the pretrained AlexNet model from torchvision.models.
-# This model was trained on over a million real-world images from ImageNet.
-# The idea is to bootstrap our CNN network weights with pretrained weights.
-# Our model will converge to a solution faster.
-# This training process is called 'fine-tuning.'
+    # get the arguments after parsing
+    args, config = parse_args()
+    if args.seed > 0:
+        print('seeding with ', args.seed)
+        random.seed(args.seed)
+        torch.manual_seed(args.seed)
 
-cnn_model = AlexNet(number_of_classes)
-cnn_model.to(device)
-print(cnn_model)
+    
+    # prepare data
+    train_dataloader, test_dataloader = prepare_data(config)
+    
+    
+    # load hyper-parameters
+    loss_fn           = nn.CrossEntropyLoss()     # criterion    
+    
+    
+    # Headsup! You should change this to the appropriate number when you fine-tune your model on a different dataset.
+    number_of_classes = config.DATASET.NUM_CLASSES
+    
+    if config.MODEL.NAME == 'alexnet':
+        model = alexnet.AlexNet(number_of_classes)
+    elif config.MODEL.NAME == 'vgg':
+        model = VGGNet(number_of_classes)
+    model.to(device)
+    print(model)
+    
+    
+    # cudnn related setting
+    '''
+    cudnn.benchmark = config.CUDNN.BENCHMARK
+    cudnn.deterministic = config.CUDNN.DETERMINISTIC
+    cudnn.enabled = config.CUDNN.ENABLED
+    gpus = list(config.GPUS)
+    '''
+    gpus  = list(config.GPUS)
+    '''
+    model = FullModel(model, criterion)
+    model = nn.DataParallel(model, device_ids=gpus).cuda()
+    logger.info(f'Using DataParallel')
+    '''
+
+    
+    
+    # optimizer
+    if config.TRAIN.OPTIMIZER == 'adam':
+        
+        optimizer         = torch.optim.Adam(model.parameters(), 
+                                             lr=config.TRAIN.BASE_LR)
+    elif config.TRAIN.OPTIMIZER == 'sgd':
+        
+        optimizer         = torch.optim.SGD(model.parameters(),
+                                    lr=config.TRAIN.BASE_LR,
+                                    momentum=config.TRAIN.MOMENTUM,
+                                    weight_decay=config.TRAIN.WEIGHT_DECAY,
+                                    nesterov=config.TRAIN.NESTEROV)
+    else:
+        raise ValueError('Only Support ADAM and SGD optimizer')
+        
+        
+    epochs              = config.TRAIN.END_EPOCH        
+    train_losses        = []
+    test_losses         = []
+    train_accuracies    = []
+    test_accuracies     = []
+    start_time          = time.time()
+    
+    for t in range(epochs):
+        print(f"Epoch {t+1}\n-------------------------------")
+        avg_train_loss, train_accuracy                    = train_loop(train_dataloader, model, loss_fn, optimizer)
+        avg_test_loss, test_accuracy, conf_matrix_test    = test_loop(test_dataloader,   model, loss_fn)
+        # save the losses and accuracies
+        train_losses.append(avg_train_loss)
+        test_losses.append(avg_test_loss)
+        train_accuracies.append(train_accuracy)
+        test_accuracies.append(test_accuracy)
+
+    print(f"{config.MODEL.NAME} model has been fine-tuned!")
+    print("Total fine-tuning time: %.3f sec" %( (time.time()-start_time)) )
+    print("Total fine-tuning time: %.3f hrs" %( (time.time()-start_time)/3600) )
+
+    '''
+    # visualizing the loss curves
+    plt.plot(range(1,epochs+1), train_losses)
+    plt.plot(range(1,epochs+1), test_losses)
+    plt.title('AlexNet average losses after each epoch')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'])
+    plt.show()
+    '''
+        
 
 
-
-# Step 5: prepare the DataLoader and select your optimizer and set the hyper-parameters for learning the model from DataLoader
-#------------------------------------------------------------------------------------------------------------------------------
-
-learning_rate     = 1e-4
-batch_size_val    = 32
-epochs            = 10
-loss_fn           = nn.CrossEntropyLoss()
-optimizer         = torch.optim.Adam(cnn_model.parameters(), lr=learning_rate)
-softmax           = nn.Softmax(dim=1) # for calculating the probability of the network prediction. It is used in train_loop() and test_loop().
-
-train_dataloader  = DataLoader(train_dataset, batch_size=batch_size_val, shuffle=True)  # shuffle the images in training set during fine-tuning
-test_dataloader   = DataLoader(test_dataset, batch_size=batch_size_val,  shuffle=False) # you don't need to shuffle test images as they are not used during training
-
-
-train_losses = []
-test_losses  = []
-train_accuracies = []
-test_accuracies = []
-start_time = time.time()
-for t in range(epochs):
-    print(f"Epoch {t+1}\n-------------------------------")
-    avg_train_loss, train_accuracy                    = train_loop(train_dataloader, cnn_model, loss_fn, optimizer)
-    avg_test_loss, test_accuracy, conf_matrix_test    = test_loop(test_dataloader,   cnn_model, loss_fn)
-    # save the losses and accuracies
-    train_losses.append(avg_train_loss)
-    test_losses.append(avg_test_loss)
-    train_accuracies.append(train_accuracy)
-    test_accuracies.append(test_accuracy)
-
-print("AlexNet model has been fine-tuned!")
-print("Total fine-tuning time: %.3f sec" %( (time.time()-start_time)) )
-print("Total fine-tuning time: %.3f hrs" %( (time.time()-start_time)/3600) )
-
-# visualizing the loss curves
-plt.plot(range(1,epochs+1), train_losses)
-plt.plot(range(1,epochs+1), test_losses)
-plt.title('AlexNet average losses after each epoch')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'])
-plt.show()
-
-'''
+main()
